dfc5688a9bc46721267d33a422fe0dcd1f8a7d22
Sergio vasquez <103544569+Sergiotrader29@users.noreply.github.com>
2023-05-11 14:53:30 -0400

    Añadiendo clase DBcontest y configurando el servicio en program

---
+++ ApplicationDbContest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TiendaAPi.Models;
+
+namespace TiendaAPi
+{
+    public class ApplicationDbContest : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContest(DbContextOptions<ApplicationDbContest> options, IConfiguration configuration)
+            : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Tienda> Tiendas { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
---
+++ Controllers/TiendasController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TiendasController : ControllerBase
+{
+    private readonly AppDbContext _dbContext;
+
+    public TiendasController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProductos(int id)
+    {
+        var tienda = _dbContext.Tiendas.Include(t => t.Productos).FirstOrDefault(t => t.ID == id);
+        if (tienda == null)
+        {
+            return NotFound();
+        }
+
+        var productos = tienda.Productos.Select(p => new
+        {
+            p.ID,
+            p.Nombre,
+            p.SKU,
+            p.Descripcion,
+            p.Valor,
+            Imagen = ConvertImageToBase64(p.Imagen)
+        });
+
+        return Ok(productos);
+    }
+
+    private string ConvertImageToBase64(string imagePath)
+    {
+        // Lógica para convertir la imagen a base64
+        // Aquí debes leer el archivo de imagen desde el sistema de archivos y convertirlo a base64
+        return "base64_encoded_image";
+    }
+}
