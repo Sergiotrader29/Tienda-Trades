6a6342709d6df79cd2648c1b3a3e3d26f5f670a7
Sergio vasquez <103544569+Sergiotrader29@users.noreply.github.com>
2023-05-11 18:40:44 -0400

    agruegue servilog para añadir los logs de los eventos GET

--- Controllers/TiendasController.cs
+++ Controllers/TiendasController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
@@ -9,23 +10,29 @@ using TiendaAPi;
 public class TiendasController : ControllerBase
 {
     private readonly ApplicationDbContest _dbContext;
+    private readonly ILogger<TiendasController> _logger;
 
-    public TiendasController(ApplicationDbContest dbContext)
+    public TiendasController(ApplicationDbContest dbContext, ILogger<TiendasController> logger)
     {
         _dbContext = dbContext;
+        _logger = logger;
     }
 
     [HttpGet("{id}")]
     public IActionResult GetProductos(int id)
     {
+        _logger.LogInformation("Se ha llamado al método GetProductos");
+
         var tienda = _dbContext.Tiendas.Include(t => t.Productos).FirstOrDefault(t => t.ID == id);
         if (tienda == null)
         {
+            _logger.LogWarning("No se encontró la tienda con ID: {ID}", id);
             return NotFound();
         }
 
         var productos = tienda.Productos.Select(p => new
         {
+            TiendaNombre = tienda.Nombre,
             p.ID,
             p.Nombre,
             p.SKU,
@@ -36,7 +43,6 @@ public class TiendasController : ControllerBase
 
         return Ok(productos);
     }
-
 
     // convertir imagen a base 64
     private string ConvertImageToBase64(string imagePath)
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 using Microsoft.EntityFrameworkCore;
 using System.Configuration;
 using System;
@@ -5,8 +9,16 @@ using TiendaAPi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configurar Serilog como el proveedor de logging
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -30,3 +42,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// Cerrar y liberar el logger de Serilog
+Log.CloseAndFlush();
